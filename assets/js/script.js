28c0b741629f3e5faf930b370c3f2dddfbf4c8b0
Gonzalo Ariel Fernández <gafernandez.web@gmail.com>
2020-05-30 20:19:17 -0300

    nmodificacion a vista serie en mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,47 @@
 const menu = document.querySelector(".js-menu");
 const nav = document.querySelector(".js-navbar");
 
+const header = document.querySelector(".js-header-wrapper");
+
 menu.onclick = clickMenu
 
-function clickMenu(){
+function clickMenu() {
     nav.classList.toggle("open");
     menu.classList.toggle("active");
 }
 
-// JS para carouseles 
+
 const carousels = document.querySelectorAll(".carousel-wrapper");
 Object.values(carousels).map(carousel => {
     const slider = new Glide(carousel, {
         type: 'slider',
         startAt: 0,
-        perView:5,
+        perView: 5,
         breakpoints: {
             1500: { perView: 4 },
-            1100: { perView: 3},
+            1100: { perView: 3 },
             740: { perView: 2 },
-            450: { perView: 1 }
+            450: { perView: 1}
         },
-        gap: 5
+        gap: 10       
+    });
+    slider.mount();
+});
+
+const carouselSeason = document.querySelectorAll(".carousel-season");
+Object.values(carouselSeason).map(carouselS => {
+    const slider = new Glide(carouselS, {
+        type: 'slider',
+        startAt: 0,
+        perView: 5,
+        breakpoints: {
+            1500: { perView: 3 },
+            1100: { perView: 3 },
+            740: { perView: 2 },
+            450: { perView: 0}
+        },
+        gap: 0,
+       
     });
     slider.mount();
 });
